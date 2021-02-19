1afb7e47448d9a78096a4abded211fa079d5784a
PatrickFlorian-dev <patrickfloriansoftwareengineer@gmail.com>
2021-02-19 03:55:31 -0500

    Smtp email config updates

--- demo_api/settings.py
+++ demo_api/settings.py
@@ -206,5 +206,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'patrickflorian2@gmail.com'
-EMAIL_HOST_PASSWORD = 'pflorian212'
+EMAIL_HOST_PASSWORD = 'jddx gpei kbxu tamt'
+# EMAIL_HOST_PASSWORD = 'pflorian212'
 EMAIL_USE_TLS = True
